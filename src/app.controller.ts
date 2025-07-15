f5eb745c0fe95ab580e8cb6708f20eecf5a3019b
thangstevebui <bui.thang@vn.lion-garden.com>
2025-07-15 13:29:07 +0700

    🎉 Refactor deployment
    refactor deployment

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,29 +1,36 @@
 import { Controller, Get } from "@nestjs/common";
-import { AppService } from "@/app.service";
-import { HealthCheckService, MemoryHealthIndicator } from "@nestjs/terminus";
-import { IsPublic } from "./_core/decorators/isPulic.decorator";
+// import {
+//   HealthCheckService,
+//   HttpHealthIndicator,
+//   MemoryHealthIndicator,
+//   MikroOrmHealthIndicator,
+// } from "@nestjs/terminus";
+import { AppService } from "./app.service";
+// import { IsPublic } from "./_core/decorators/isPulic.decorator";
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
 
-    private health: HealthCheckService,
-    private memory: MemoryHealthIndicator,
+    // private health: HealthCheckService,
+    // private http: HttpHealthIndicator,
+    // private db: MikroOrmHealthIndicator,
+    // private memory: MemoryHealthIndicator,
   ) {}
 
-  @Get("/hello")
-  @IsPublic()
+  @Get("")
   getHello(): string {
     return this.appService.getHello();
   }
 
-  @Get("/health")
-  @IsPublic()
-  checkHealth() {
-    console.log("Health check initiated");
-    return this.health.check([
-      () => this.memory.checkHeap("memory_heap", 150 * 1024 * 1024),
-    ]);
-  }
+  // @Get("/health")
+  // // @IsPublic()
+  // checkHealth() {
+  //   return this.health.check([
+  //     () => this.db.pingCheck("database"),
+  //
+  //     () => this.memory.checkHeap("memory_heap", 150 * 1024 * 1024),
+  //   ]);
+  // }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,87 +1,21 @@
+import "dotenv/config";
 import { NestFactory } from "@nestjs/core";
-import { AppModule } from "@/app.module";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
-import {
-  ConsoleLogger,
-  ForbiddenException,
-  Logger,
-  ValidationPipe,
-} from "@nestjs/common";
-// import * as cookieParser from "cookie-parser";
-import { json, urlencoded } from "express";
-import helmet from "helmet";
-// import * as compression from "compression";
-import { TimeoutInterceptor } from "@/_core/interceptors/timeout.interceptor";
-import { GLOBAL_MESSAGES } from "@/_core/constants/common.constants";
+import { AppModule } from "./app.module";
 
-async function bootstrap(): Promise<void> {
-  //Logger
-  const logger = new Logger();
+async function bootstrap() {
+  const PORT = process.env.PORT ?? 3000;
 
-  // Application
-  const app = await NestFactory.create(AppModule, {
-    logger: new ConsoleLogger({
-      prefix: "An Management",
-    }),
-  });
-  app.setGlobalPrefix("api/");
+  const NODE_ENV = process.env.NODE_ENV ?? "development";
 
-  // Interceptor
-  app.useGlobalInterceptors(new TimeoutInterceptor(1000 * 3));
+  if (NODE_ENV === "production") {
+    console.log("Running in production mode");
+  } else {
+    console.log("Running in development mode");
+  }
 
-  //Validate
-  // app.useGlobalPipes(new I18nValidationPipe());
-  app.useGlobalPipes(new ValidationPipe());
-
-  // Swagger
-  const config = new DocumentBuilder()
-    .setTitle("An Management API DOCUMENT")
-    .setDescription("The API description")
-    .setVersion("2.0")
-    .addBearerAuth({
-      type: "http",
-      name: "Authorization",
-      in: "header",
-    })
-    .addGlobalParameters({
-      name: "client_id",
-      in: "header",
-      // required: true,
-    })
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("docs", app, document);
-
-  const whitelist = Array.from(
-    (process.env.CORS_ORIGIN || "").split(",").map((el) => el.trim()),
-  );
-  app.enableCors({
-    origin: (origin: any, callback: any) => {
-      if (whitelist.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        throw new ForbiddenException(GLOBAL_MESSAGES.ORIGIN_NOT_IN_WHITELIST);
-      }
-    },
-    credentials: true,
-    allowedHeaders:
-      "Origin, X-CSRF-TOKEN, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe, channel, request-id, Authorization, X-LANG",
-    methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS,PATCH",
-  });
-
-  // app.use(cookieParser());
-  app.use(json({ limit: "10mb" }));
-  app.use(urlencoded({ limit: "10mb", extended: true }));
-  app.use(helmet());
-  // app.use(compression());
-
-  // Start Application
-  await app.listen(process.env.PORT ?? 5621, () => {
-    logger.log(
-      `🚀 🔥 🔥 🔥 Application listen on port ${process.env.PORT} 🎧`,
-      "SERVER",
-    );
+  const app = await NestFactory.create(AppModule);
+  await app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
   });
 }
 bootstrap();
