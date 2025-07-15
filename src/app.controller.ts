d7b204c183a45218d0aa81de3ed6ff4478978150
thangstevebui <bui.thang@vn.lion-garden.com>
2025-07-15 16:30:49 +0700

    🎉 Refactor
    refactor

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,60 +1,16 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Post,
-  Put,
-} from '@nestjs/common';
-import { UserService } from './app.service';
-import { User } from 'type';
+import { Controller, Get } from "@nestjs/common";
+import { ApiExcludeEndpoint } from "@nestjs/swagger";
+import { AppService } from "./app.service";
+import { IsPublic } from "./_core/decorators/isPulic.decorator";
 
-@Controller('user')
-export class UserController {
-  constructor(private readonly userService: UserService) {}
-  // get all users
-  @Get('all')
-  getAllUsers(): User[] | undefined {
-    return this.userService.getAllUsers();
-  }
+@Controller()
+export class AppController {
+  constructor(private readonly _coreService: AppService) {}
 
-  // get user by id
-  @Get(':id')
-  getUserByID(@Param('id') id: string): User | undefined | { message: string } {
-    const userID = +id;
-    return this.userService.getUserByID(userID);
-  }
-
-  // create a user
-  @Post('create')
-  createUser(@Body() user: Partial<User>): User | { message: string } {
-    const userData = user;
-    if (
-      !userData.name ||
-      !userData.email ||
-      !userData.phone ||
-      !userData.imageUrl
-    ) {
-      return { message: 'Name, email, phone, and imageUrl are required' };
-    }
-    return this.userService.createUser(userData);
-  }
-
-  // update a user
-  @Put('update/:id')
-  updateUser(
-    @Param('id') id: string,
-    @Body() user: Partial<User>,
-  ): User | undefined | { message: string } {
-    return this.userService.updateUser(+id, user);
-  }
-
-  // delete a user
-  @Delete('delete/:id')
-  deleteUserByID(
-    @Param('id') id: string,
-  ): User[] | undefined | { message: string } {
-    return this.userService.deleteUserByID(+id);
+  @ApiExcludeEndpoint()
+  @Get()
+  @IsPublic()
+  getHello(): string {
+    return this._coreService.getHello();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
-import 'dotenv/config';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ForbiddenException, ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { TimeoutInterceptor } from './_core/interceptors/timeout.interceptor';
-import { GLOBAL_MESSAGES } from './_core/constants/common.constants';
+import "dotenv/config";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ForbiddenException, ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { TimeoutInterceptor } from "./_core/interceptors/timeout.interceptor";
+import { GLOBAL_MESSAGES } from "./_core/constants/common.constants";
 
 async function bootstrap() {
   const PORT = process.env.PORT ?? 3000;
   const app = await NestFactory.create(AppModule);
   console.log(`Environment: ${process.env.NODE_ENV}`);
 
-  app.setGlobalPrefix('api/');
+  app.setGlobalPrefix("api/");
 
   // Interceptor
   app.useGlobalInterceptors(new TimeoutInterceptor(1000 * 3));
@@ -20,28 +20,30 @@ async function bootstrap() {
   // app.useGlobalPipes(new I18nValidationPipe());
   app.useGlobalPipes(new ValidationPipe());
 
-  // Swagger
-  const config = new DocumentBuilder()
-    .setTitle('Nestjs Base API DOCUMENT')
-    .setDescription('The Nestjs Base API description')
-    .setVersion('2.0')
-    .addBearerAuth({
-      type: 'http',
-      name: 'Authorization',
-      in: 'header',
-    })
-    .addGlobalParameters({
-      name: 'client_id',
-      in: 'header',
-      // required: true,
-    })
-    .build();
+  if (process.env.NODE_ENV === "development") {
+    // Swagger
+    const config = new DocumentBuilder()
+      .setTitle("Nestjs Base API DOCUMENT")
+      .setDescription("The Nestjs Base API description")
+      .setVersion("2.0")
+      .addBearerAuth({
+        type: "http",
+        name: "Authorization",
+        in: "header",
+      })
+      .addGlobalParameters({
+        name: "client_id",
+        in: "header",
+        // required: true,
+      })
+      .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("docs", app, document);
+  }
 
   const whitelist = Array.from(
-    (process.env.CORS_ORIGIN || '').split(',').map((el) => el.trim()),
+    (process.env.CORS_ORIGIN || "").split(",").map((el) => el.trim()),
   );
   app.enableCors({
     origin: (origin: any, callback: any) => {
@@ -53,8 +55,8 @@ async function bootstrap() {
     },
     credentials: true,
     allowedHeaders:
-      'Origin, X-CSRF-TOKEN, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe, channel, request-id, Authorization, X-LANG',
-    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS,PATCH',
+      "Origin, X-CSRF-TOKEN, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe, channel, request-id, Authorization, X-LANG",
+    methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS,PATCH",
   });
 
   await app.listen(PORT, () => {
